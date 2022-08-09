d08c3ffd2e4d05ab114f894b5ce0e9ca53aae8e7
Abiola Olamilekan <abiolawalter2015@gmail.com>
2022-08-09 12:21:53 +0100

    implemented passwordless signIn authentication

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Header from "./components/Header";
+import RegisterComplete from "./pages/auth/RegisterComplete";
 
 // External import
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
     return (
         <>
             <Header />
+            <ToastContainer />
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/login" element={<Login />} />
                 <Route exact path="/register" element={<Register />} />
+                <Route
+                    exact
+                    path="/register/complete"
+                    element={<RegisterComplete />}
+                />
             </Routes>
         </>
     );
--- src/pages/auth/RegisterComplete.js
+++ src/pages/auth/RegisterComplete.js
@@ -1,44 +1,37 @@
 // Internal import
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { auth } from "../../utils/firebase";
 import { sendSignInLinkToEmail } from "firebase/auth";
 
 // External import
 import { toast } from "react-toastify";
+// import { useHistory } from "react-router-dom";
 
-const Register = () => {
+const RegisterComplete = () => {
     return (
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <h4>Register</h4>
-                    <RegisterForm />
+                    <CompleteRegistrationForm />
                 </div>
             </div>
         </div>
     );
 };
 
-const RegisterForm = () => {
+const CompleteRegistrationForm = () => {
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        setEmail(window.localStorage.getItem("emailForRegistration"));
+    }, []);
+
+    // let history = useHistory();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        const config = {
-            url: process.env.REACT_APP_REGISTER_URL,
-            handleCodeInApp: true,
-        };
-
-        // sendSignInLinkToEmail
-        await sendSignInLinkToEmail(auth, email, config);
-        toast.success(
-            `An Email has been sent to ${email}. Click the  link to complete your registration`
-        );
-        // Save user email in local storage
-        window.localStorage.setItem("emailForRegistration", email);
-        // Clear the state
-        setEmail("");
     };
 
     return (
@@ -47,16 +40,26 @@ const RegisterForm = () => {
                 type="email"
                 className="form-control"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                autoFocus
-                placeholder="Enter a valid email address"
+                disabled
             />
 
+            <input
+                type="password"
+                className="form-control"
+                value={password}
+                autoFocus
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                }}
+                placeholder="Enter a valid password"
+            />
+            <br />
+
             <button type="submit" className="btn btn-raised">
-                Register
+                Complete Registration
             </button>
         </form>
     );
 };
 
-export default Register;
+export default RegisterComplete;
